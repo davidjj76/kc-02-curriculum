8b29807b1cb58426cc4733ce40815804bc3a4096
David Jiménez <davidjj76@gmail.com>
2017-01-02 02:04:31 +0100

    Añadido el video

--- js/accordion.js
+++ js/accordion.js
@@ -13,10 +13,5 @@ for (var i = 0; i < acc.length; i++) {
 function toggleAccordion(element) {
     element.classList.toggle('active');
     element.nextElementSibling.classList.toggle('show');
-    if (element.classList.contains('active')) {
-    	activeAccordion = element;
-    } else {
-    	activeAccordion = null;
-    }
-
+    activeAccordion = element.classList.contains('active') ? element : null;
 }
